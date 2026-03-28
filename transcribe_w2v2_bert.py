863daab209185b84005a30adc7c44113fa410831
Dmitrii Nikulin <nikulas.nikulin@yandex.ru>
2026-03-28 21:40:31 +0300

    rm extra asr

--- transcribe_w2v2_bert.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Compatibility wrapper for Wav2Vec2-BERT transcription."""
-
-from transcription_tools.transcribe_w2v2_bert import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
